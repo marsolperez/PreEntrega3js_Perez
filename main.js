69260c8def5c5e0965d9ac25a749b05a44b41465
marsolperez <124072967+marsolperez@users.noreply.github.com>
2023-05-08 17:47:43 -0300

    arregle formulario con boton cerrar sesion y arrege funcionalidad carrito

--- main.js
+++ main.js
@@ -1,37 +1,103 @@
-// capturo info del formulario dom
+// // capturo info del formulario dom
+// const loginForm = document.getElementById('login-form');
+
+// // Agregamos el evento 'submit' al formulario
+// loginForm.addEventListener('submit', (event) => {
+//     event.preventDefault();// Prevenir la que se borren los datos 
+
+//     // Obtenemos los valores de los campos de entrada
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+
+//     //Verificar si los campos están llenos
+//     if (username && password) {
+//         const nombreUsuario = document.querySelector('#nombreUsuario');
+//         nombreUsuario.textContent = `${username}!`;
+
+//     } else {
+//         //mensaje de error
+//         const nombreUsuario = document.querySelector('#nombreUsuario');
+//         nombreUsuario.textContent = 'Ingrese su nombre de usuario y contraseña';
+//     }
+
+//     //  objeto JSON con los valores de los campos de entrada
+//     const user = {
+//         username: username,
+//         password: password
+//     };
+
+
+//     // Almacenar el objeto JSON en el localStorage
+//     localStorage.setItem('user', JSON.stringify(user));
+// });
+
+// Verificar si el usuario ya ha iniciado sesión
+window.addEventListener('DOMContentLoaded', () => {
+    const storedUser = localStorage.getItem('user');
+
+    if (storedUser) {
+        const user = JSON.parse(storedUser);
+        const nombreUsuario = document.querySelector('#nombreUsuario');
+        nombreUsuario.textContent = `${user.username}!`;
+
+        // Ocultar el formulario de inicio de sesión
+        const loginForm = document.getElementById('login-form');
+        loginForm.style.display = 'none';
+    }
+});
+
+// Capturar información del formulario DOM
 const loginForm = document.getElementById('login-form');
 
-// Agregamos el evento 'submit' al formulario
+// Agregar el evento 'submit' al formulario
 loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();// Preveniir la que se borren los datos 
+    event.preventDefault(); // Prevenir que se borren los datos 
 
-    // Obtenemos los valores de los campos de entrada
-    // const nombreUsuario = document.querySelector('#nombreUsuario');
+    // Obtener los valores de los campos de entrada
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-
-    //Verificar si los campos están llenos
+    // Verificar si los campos están llenos
     if (username && password) {
         const nombreUsuario = document.querySelector('#nombreUsuario');
         nombreUsuario.textContent = `${username}!`;
 
+        // Crear objeto JSON con los valores de los campos de entrada
+        const user = {
+            username: username,
+            password: password
+        };
+
+        // Almacenar el objeto JSON en el localStorage
+        localStorage.setItem('user', JSON.stringify(user));
+
+        // Ocultar el formulario de inicio de sesión
+        loginForm.style.display = 'none';
     } else {
-        //mensaje de error
+        // Mostrar mensaje de error
         const nombreUsuario = document.querySelector('#nombreUsuario');
         nombreUsuario.textContent = 'Ingrese su nombre de usuario y contraseña';
     }
-
-    //  objeto JSON con los valores de los campos de entrada
-    const user = {
-        username: username,
-        password: password
-    };
-
-
-    // Almacenar el objeto JSON en el localStorage
-    localStorage.setItem('user', JSON.stringify(user));
 });
+
+// Agregar el botón para cerrar sesión
+const logoutButton = document.getElementById('logout-button');
+
+// Agregar el evento 'click' al botón
+logoutButton.addEventListener('click', () => {
+    // Eliminar el usuario del localStorage
+    localStorage.removeItem('user');
+
+    // Mostrar el formulario de inicio de sesión
+    loginForm.style.display = 'block';
+
+    // Limpiar el campo de nombre de usuario
+    const nombreUsuario = document.querySelector('#nombreUsuario');
+    nombreUsuario.textContent = '';
+});
+
+
 
 //nivel de experiencia
 
@@ -47,13 +113,13 @@ nivelSelect.addEventListener('change', () => {
     localStorage.setItem('nivel', nivelValue);
 });
 
+
 // Carrito de Compras
 //obtener archivos del dom
 
-const agregarAlCarritoBtns = document.querySelectorAll('.addCarrito');
-
-agregarAlCarritoBtns.forEach(addCarritoButton => {
-    addCarritoButton.addEventListener('click', addCarritoClicked);
+const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
+addToShoppingCartButtons.forEach((addToCartButton) => {
+    addToCartButton.addEventListener('click', addToCartClicked);
 });
 
 //crear Variables /comprar.sumar carrito 
@@ -61,54 +127,50 @@ agregarAlCarritoBtns.forEach(addCarritoButton => {
 const comprarButton = document.querySelector('.comprarButton');
 comprarButton.addEventListener('click', comprarButtonClicked);
 
-
 const shoppingCartItemsContainer = document.querySelector( '.shoppingCartItemsContainer');
-
 // funcion agregar al carrito
 
-function addCarritoClicked(event) {
+function addToCartClicked(event) {
     const button = event.target;
     const item = button.closest('.item');
 
-    const itemTitulo = item.querySelector('.item-titulo').textContent;
-    const itemPrecio = item.querySelector('.item-precio').textContent;
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
     const itemImage = item.querySelector('.item-image').src;
 
-    addItemCarrito(itemTitulo, itemPrecio, itemImage);
-
+    addItemToShoppingCart(itemTitle, itemPrice, itemImage);
 }
-
 // crear funcion que sume todos los elementos
-function addItemCarrito(itemTitulo, itemPrecio, itemImage) {
-    const elementosTitulo = shoppingCartItemsContainer.getElementsByClassName(
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+    const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
         'shoppingCartItemTitle'
     );
-    for (let i = 0; i < elementosTitulo.length; i++) {
-        if (elementosTitulo[i].innerText === itemTitle) {
-            let elementQuantity = elementosTitulo[
+    for (let i = 0; i < elementsTitle.length; i++) {
+        if (elementsTitle[i].innerText === itemTitle) {
+            let elementQuantity = elementsTitle[
                 i
             ].parentElement.parentElement.parentElement.querySelector(
                 '.shoppingCartItemQuantity'
             );
             elementQuantity.value++;
             $('.toast').toast('show');
-            sumarCarritoTotal();
+            updateShoppingCartTotal();
             return;
         }
     }
 
-    const filaCarritoDeCompra = document.createElement('div');
-    const carritoComprasContenido = `
+    const filaCarrito = document.createElement('div');
+    const shoppingCartContent = `
     <div class="row shoppingCartItem">
         <div class="col-6">
             <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                 <img src=${itemImage} class="shopping-cart-image">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitulo}</h6>
+                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
             </div>
         </div>
         <div class="col-2">
             <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrecio}</p>
+                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
             </div>
         </div>
         <div class="col-4">
@@ -120,71 +182,66 @@ function addItemCarrito(itemTitulo, itemPrecio, itemImage) {
             </div>
         </div>
     </div>`;
-    filaCarritoDeCompra.innerHTML = carritoComprasContenido;
-    shoppingCartItemsContainer.append(filaCarritoDeCompra);
-}
-// crear funcion para eliminar item del caarrito
+    filaCarrito.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(filaCarrito);
 
-    filaCarritoDeCompra
+    // crear funcion para eliminar item del caarrito
+
+    filaCarrito
         .querySelector('.buttonDelete')
-        .addEventListener('click', eliminarItemCarrito);
+        .addEventListener('click', borrarItemCarrito);
 
-    
-    filaCarritoDeCompra 
+        filaCarrito
         .querySelector('.shoppingCartItemQuantity')
-        .addEventListener('change', cantidadModificada);
+        .addEventListener('change', quantityChanged);
 
-        sumarCarritoTotal();
+    updateShoppingCartTotal();
 
+    //Crear Funcion que sume los totales de los items seleccionados
+}
 
-
-//Crear Funcion que sume los totales de los items seleccionados
-
-function sumarCarritoTotal() {
+function updateShoppingCartTotal() {
     let total = 0;
     const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
 
-    const itemsCarritoCompras = document.querySelectorAll('.shoppingCartItem');
+    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
 
-    itemsCarritoCompras.forEach((shoppingCartItem) => {
-        const carritoItemPrecioElemento = shoppingCartItem.querySelector(
-            '.carritoItemPrecio'
+    shoppingCartItems.forEach((shoppingCartItem) => {
+        const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
+            '.shoppingCartItemPrice'
         );
-        const carritoItemPrecio = Number(
-            carritoItemPrecioElemento.textContent.replace('$', '')
+        const shoppingCartItemPrice = Number(
+            shoppingCartItemPriceElement.textContent.replace('$', '')
         );
-
-        const carritoItemCantidadElemento = shoppingCartItem.querySelector(
-            'carritoItemCantidad'
+        const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
+            '.shoppingCartItemQuantity'
         );
-        const carritoItemCantidad = Number(
-            carritoItemCantidadElemento.value
+        const shoppingCartItemQuantity = Number(
+            shoppingCartItemQuantityElement.value
         );
-        total = total + carritoItemPrecio * carritoItemCantidad;
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
     });
     shoppingCartTotal.innerHTML = `${total.toFixed(2)}$`;
-
-
 }
 
 //funcion remover items
-function eliminarItemCarrito(event) {
-    const buttonclicked = event.target;
-    buttonclicked.closest ('.shoppingCartItem').remove(); 
-    sumarCarritoTotal();
+function borrarItemCarrito(event) {
+    const buttonClicked = event.target;
+    buttonClicked.closest('.shoppingCartItem').remove();
+    updateShoppingCartTotal();
 }
 
 //Funcion modificar cantidades
 
-function cantidadModificada(event) {
+function quantityChanged(event) {
     const input = event.target;
     input.value <= 0 ? (input.value = 1) : null;
-    sumarCarritoTotal();
+    updateShoppingCartTotal();
 }
 
 //crear funcion comprar
 
 function comprarButtonClicked() {
     shoppingCartItemsContainer.innerHTML = '';
-    sumarCarritoTotal();
+    updateShoppingCartTotal();
 }
